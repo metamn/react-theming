cb0cbad742d6552fbf235e3e6e860c4accd9ef3d
metamn <bartus.csongor@gmail.com>
2018-12-15 11:02:39 +0200

    Working on composition

--- src/composing/App.js
+++ src/composing/App.js
@@ -17,8 +17,22 @@ const Button3 = styled(Button)`
 	${buttonWithBorder}
 `
 
+const buttonWithBorderProps = props => css`
+	border: ${props.border};
+`
+
+const Button4 = styled(Button)`
+	${buttonWithBorderProps}
+`
+
+const Button5 = styled(Button)`
+	${buttonWithBorderProps({
+		border: "4px dashed red"
+	})}
+`
+
 const Container = styled.div`
-	 > * + * {
+	 > * {
 		margin: 1.25em
 	}
 `
@@ -33,6 +47,8 @@ class App extends React.Component {
 				<Button2 theme={theme}>Inverted</Button2>
 				<Button3 theme={theme}>With border</Button3>
 				<Button3>With border, without theme</Button3>
+				<Button4 theme={theme} border="2px dotted">With styled border</Button4>
+				<Button5 theme={theme}>With dynamically styled border</Button5>
 			</Container>
 		)
 	}
